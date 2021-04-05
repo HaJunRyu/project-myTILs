dc24f72ad5d23e3e763fe1e32c1a69daac7e5130
bcround <sbc1142@gmail.com>
2021-04-05 16:34:06 +0900

    feat: Use thunk creator functions in Toast UI Editor and create ToastUIPage

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
+import tils from './tils';
 
-const rootReducer = combineReducers({ auth });
+const rootReducer = combineReducers({ auth, tils });
 
 export default rootReducer;
